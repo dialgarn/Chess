d107c5fce6aa14baac8fdc6c951b138af29655e5
dialgarn <dialgarn@gmail.com>
2024-02-27 20:27:30 -0700

    sadness

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -12,4 +12,5 @@ public interface GameDAO {
 
     void clear();
 
+    int getSize();
 }
--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -50,4 +50,8 @@ public class MemoryGameDAO implements GameDAO {
     public void clear() {
         gameList.clear();
     }
+
+    public int getSize() {
+        return gameList.size();
+    }
 }
