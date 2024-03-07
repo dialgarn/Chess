e864e82d20c530c773258582c8a65d3bc1a501ad
dialgarn <dialgarn@gmail.com>
2024-03-07 13:37:12 -0700

    finished creating unit tests for gameDAO

--- server/src/test/java/dataAccessTests/DatabaseGameDAOTest.java
+++ server/src/test/java/dataAccessTests/DatabaseGameDAOTest.java
@@ -1,15 +1,16 @@
 package dataAccessTests;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import dataAccess.DataAccessException;
 import dataAccess.DatabaseAuthDAO;
 import dataAccess.DatabaseGameDAO;
 import dataAccess.DatabaseUserDAO;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
 
 class DatabaseGameDAOTest {
 
@@ -40,32 +41,97 @@ class DatabaseGameDAOTest {
 
     @Test
     void createGameSuccess() {
+        int gameID = Assertions.assertDoesNotThrow(()->gameDao.createGame("testGame"));
+
+        Assertions.assertEquals(1, gameID);
     }
 
     @Test
-    void createGameFailure() {
+    void createGameFailure() throws DataAccessException {
+        UserData newUser = new UserData("newUser", "test", "test@gmail.com");
 
+        userDao.registerUser(newUser);
+
+        AuthData fakeAuth = new AuthData("i am a sneaky man", "newUser");
+
+        Assertions.assertThrows(DataAccessException.class, ()->authDao.verify(fakeAuth.authToken()));
+
+        int gameID = Assertions.assertDoesNotThrow(()->gameDao.createGame("testGame"));
+
+        Assertions.assertEquals(1, gameID);
     }
 
     @Test
-    void joinGameSuccess() {
+    void joinGameSuccess() throws DataAccessException {
+        UserData newUser = new UserData("newUser", "test", "test@gmail.com");
+
+        userDao.registerUser(newUser);
+
+        authDao.createAuth(newUser);
+
+        int gameID = gameDao.createGame("testGame");
+
+        Assertions.assertDoesNotThrow(()->gameDao.joinGame(gameID, ChessGame.TeamColor.WHITE, "newUser"));
     }
 
     @Test
-    void joinGameFailure() {
+    void joinGameFailure() throws DataAccessException {
+        UserData newUser = new UserData("newUser", "test", "test@gmail.com");
 
+        userDao.registerUser(newUser);
+
+        authDao.createAuth(newUser);
+
+        int gameID = gameDao.createGame("testGame");
+        int badGameID = gameID + 1;
+
+
+        Assertions.assertThrows(DataAccessException.class, ()->gameDao.joinGame(badGameID, ChessGame.TeamColor.WHITE, "testUser"));
+        Assertions.assertThrows(DataAccessException.class, ()->gameDao.joinGame(gameID, ChessGame.TeamColor.WHITE, "unregisteredUser"));
     }
 
     @Test
-    void listGamesSuccess() {
+    void listGamesSuccess() throws DataAccessException {
+        UserData newUser = new UserData("newUser", "test", "test@gmail.com");
+
+        userDao.registerUser(newUser);
+
+        authDao.createAuth(newUser);
+        int gameID = gameDao.createGame("testGame");
+        gameDao.joinGame(gameID, ChessGame.TeamColor.WHITE, "newUser");
+
+        String expectedOutput = String.format("{\"games\":[{\"gameID\":%d,\"whiteUsername\":\"%s\",\"gameName\":\"%s\",\"game\":{}}]}"
+                , gameID, "newUser", "testGame");
+
+        String myOutput = new Gson().toJson(Map.of("games", gameDao.listGames()));
+        Assertions.assertEquals(expectedOutput, myOutput);
     }
 
     @Test
-    void listGameFailure() {
-        
+    void listGameFailure() throws DataAccessException {
+        UserData newUser = new UserData("newUser", "test", "test@gmail.com");
+
+        userDao.registerUser(newUser);
+
+        authDao.createAuth(newUser);
+        int gameID = gameDao.createGame("testGame");
+        gameDao.joinGame(gameID, ChessGame.TeamColor.WHITE, "newUser");
+
+        AuthData fakeAuth = new AuthData("i am a sneaky man", "newUser");
+
+        Assertions.assertThrows(DataAccessException.class, ()->authDao.deleteAuth(fakeAuth.authToken()));
+
+        String badOutput = String.format("{\"games\":[{\"gameID\":%d,\"whiteUsername\":\"%s\",\"gameName\":\"%s\",\"game\":{}}]}"
+                , gameID, null, "testGame");
+
+        String myOutput = new Gson().toJson(Map.of("games", gameDao.listGames()));
+        Assertions.assertNotEquals(badOutput, myOutput);
     }
 
     @Test
-    void clear() {
+    void clear() throws DataAccessException {
+        gameDao.createGame("newGame");
+
+        Assertions.assertDoesNotThrow(()->gameDao.clear());
     }
 }
